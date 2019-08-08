8fb08b030a219d3032253adf75195e804f7e3d00
Dylan Bickerstaff <dylan@belowaverage.org>
2019-08-07 22:22:13 -0400

    Version

--- WS5SuperBooter/Properties/AssemblyInfo.cs
+++ WS5SuperBooter/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WS5SuperBooter")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Dylan Bickerstaff")]
+[assembly: AssemblyConfiguration("Dylan Bickerstaff")]
+[assembly: AssemblyCompany("Dylan Bickerstaff")]
 [assembly: AssemblyProduct("WS5SuperBooter")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Dylan Bickerstaff")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
